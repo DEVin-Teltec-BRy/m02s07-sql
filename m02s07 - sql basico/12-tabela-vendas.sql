58b6f7204b1a152f58a56228260a8ad765f8219e
Michael Nascimento <michael.nsc@outlook.com>
2022-01-27 21:40:07 -0300

    aula 04 sm 07 md02

--- m02s07 - sql basico/12-tabela-vendas.sql
+++ m02s07 - sql basico/12-tabela-vendas.sql
@@ -1,5 +1,9 @@
+drop table vendas;
+
+SELECT * FROM vendas;
+
 create table vendas (
-  numero_pedido INT NOT NULL UNIQUE,
+  numero_pedido SERIAL NOT NULL UNIQUE,
   cliente VARCHAR(80) NOT NULL,
   data_pedido TIMESTAMP NOT NULL,
   valor_total NUMERIC NOT NULL CHECK(valor_total > 0),
@@ -11,146 +15,124 @@ create table vendas (
   cupom_desconto VARCHAR(10)
 );
 
-insert into vendas
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
+alter table vendas
+ADD CONSTRAINT vendaspk PRIMARY KEY (numero_pedido);
+
+insert into vendas 
+(cliente, data_pedido, valor_total, qtd_itens)
 values
-(88, 'Michael', '2022-01-26 12:00:00', 15.00, 1),
-(89, 'Fulano', '2022-01-24 17:15:00', 155.99, 8 ),
-(90, 'Beltrano', '2022-01-04 19:00:00', 14.30, 2 );
+('Michael', '2022-01-26 12:00:00', 15.00, 1),
+('Fulano', '2022-01-24 17:15:00', 155.99, 8 ),
+('Beltrano', '2022-01-04 19:00:00', 14.30, 2 ),
+ -- DANIEL
+('João da silva', '2021-12-24 17:30:00', 254.30, 10),
+('Maria da silva', '2021-12-23 15:30:00', 135.25, 25),
+('Mario da silva', '2021-12-22 16:30:00', 149.60, 5),
+ -- Bruno
+('Juca', '2022-01-26 11:00:00', 50.00, 2),
+('Bruno', '2022-01-25 17:30:00', 20.00, 1),
+('Joana', '2022-01-22 15:15:00', 60.00, 3),
+ -- MATHEUS
+('Matheus', '2022-02-17 13:37:48', 12.99, 3),
+('Matheus', '2022-04-26 03:00:12', 1.00, 100),
+( 'Matheus', '2023-05-22 21:17:55', 9983.03, 1),
+ -- VITOR
+('Jubileu', '2022-01-26 12:00:00', 1.00, 1),
+('Schwarzenegger', '1984-01-24 03:15:00', 0.01, 8 ),
+('Chandler', '2022-01-04 19:00:00', 19.50, 2 ),
+ -- Icaro
+( 'Frederico','2022-01-15 15:30:00', 97, 1),
+('mariazinha','2022-01-17 17:55:33', 50, 1),
+('joaquim','2022-01-05 18:35:33', 597, 5),
+ -- Priscilla
+('Piscilla', '2022-01-28 16:00:00', 120.00, 1),
+('Joyce', '2022-01-26 12:00:00', 15.00, 1),
+('Daiane', '2022-01-24 17:15:00', 155.99, 8 ),
+('Jonas', '2022-01-04 19:00:00', 14.30, 2 ),
+ -- Sabrina
+('Joao Souza', '26-01-2022 11:00:00', 100, 2),
+('Pedro Silva', '28-01-2022 08:00:00', 250, 5),
+('Ana Hering', '25-01-2022 10:00:00', 400, 7),
+ -- Guilherme
+('Mario Bros', '2022-05-20 19:40:48', 950.00, 3),
+('Sonic Hedgehog', '2022-04-01 07:00:00', 263.50, 4),
+('Solid Snake', '2023-01-22 20:30:00', 741.05, 6);
 
--- DANIEL
-INSERT INTO vendas 
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-VALUES (250, 'João da silva', '2021-12-24 17:30:00', 254.30, 10),
-(251, 'Maria da silva', '2021-12-23 15:30:00', 135.25, 25),
-(252, 'Mario da silva', '2021-12-22 16:30:00', 149.60, 5);
+select numero_pedido pedido, cliente, valor_total total from vendas;
 
--- Bruno
-INSERT INTO vendas (
-    numero_pedido, cliente, data_pedido, valor_total, qtd_itens, data_pagamento)
-VALUES
-(120, 'Juca', '2022-01-26 11:00:00', 50.00, 2, '2022-01-26 11:05:00'),
-(121, 'Bruno', '2022-01-25 17:30:00', 20.00, 1, '2022-01-25 17:35:00'),
-(122, 'Joana', '2022-01-22 15:15:00', 60.00, 3, '2022-01-22 15:20:00');
+-- select DISTINCT numero_pedido, entregue, data_entrega from vendas;
 
--- MATHEUS
-INSERT INTO vendas 
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-VALUES
-( 517, 'Matheus', '2022-02-17 13:37:48', 12.99, 3),
-( 776, 'Matheus', '2022-04-26 03:00:12', 1.00, 100),
-( 1154, 'Matheus', '2023-05-22 21:17:55', 9983.03, 1);
+-- select * 
+-- from vendas WHERE valor_total > 200;
 
--- VITOR
-insert into vendas
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-values
-(555, 'Jubileu', '2022-01-26 12:00:00', 1.00, 1),
-(666, 'Schwarzenegger', '1984-01-24 03:15:00', 0.01, 8 ),
-(777, 'Chandler', '2022-01-04 19:00:00', 19.50, 2 );
+-- select cliente, numero_pedido, qtd_itens 
+-- from vendas 
+-- WHERE qtd_itens < 5;
 
--- Icaro
-insert into vendas
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-values
-(87, 'Frederico','2022-01-15 15:30:00', 97, 1),
-(86,'mariazinha','2022-01-17 17:55:33', 50, 1),
-(55,'joaquim','2022-01-05 18:35:33', 597, 5);
+-- select cliente, numero_pedido, data_pedido 
+-- from vendas 
+-- WHERE data_pedido >= '2022-01-15';
 
--- Priscilla
-INSERT INTO vendas (numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-VALUES 
-(191, 'Piscilla', '2022-01-28 16:00:00', 120.00, 1),
-(188, 'Joyce', '2022-01-26 12:00:00', 15.00, 1),
-(189, 'Daiane', '2022-01-24 17:15:00', 155.99, 8 ),
-(190, 'Jonas', '2022-01-04 19:00:00', 14.30, 2 );
+-- select cliente, numero_pedido, data_pedido 
+-- from vendas 
+-- WHERE data_pedido < '2022-01-15';
 
--- Sabrina
-insert into vendas VALUES
-(1, 'Joao Souza', '26-01-2022 11:00:00', 100, 2, true, '26-01-2022 14:00:00', false, '27-01-2022', NULL),
-(2, 'Pedro Silva', '28-01-2022 08:00:00', 250, 5, false, '28-01-2022 16:00:00', false, '30-01-2022', 'DESC10%'),
-(3, 'Ana Hering', '25-01-2022 10:00:00', 400, 7, true, '26-01-2022 10:00:00', false, '29-01-2022', NULL);
+-- select cliente, numero_pedido, data_pedido from vendas where cliente = 'Matheus';
 
+-- select numero_pedido,
+--        cliente,
+--        valor_total 
+-- from vendas 
+-- where pago is true;
 
--- Guilherme
-INSERT INTO vendas 
-(numero_pedido, cliente, data_pedido, valor_total, qtd_itens)
-VALUES
-( 159, 'Mario Bros', '2022-05-20 19:40:48', 950.00, 3),
-( 146, 'Sonic Hedgehog', '2022-04-01 07:00:00', 263.50, 4),
-( 197, 'Solid Snake', '2023-01-22 20:30:00', 741.05, 6);
+-- select * from vendas;
 
-select * 
-from vendas WHERE valor_total > 200;
-
-select cliente, numero_pedido, qtd_itens 
-from vendas 
-WHERE qtd_itens < 5;
-
-select cliente, numero_pedido, data_pedido 
-from vendas 
-WHERE data_pedido >= '2022-01-15';
-
-select cliente, numero_pedido, data_pedido 
-from vendas 
-WHERE data_pedido < '2022-01-15';
-
-select cliente, numero_pedido, data_pedido from vendas where cliente = 'Matheus';
-
-select numero_pedido,
-       cliente,
-       valor_total 
-from vendas 
-where pago is true;
-
-select * from vendas;
-
-SELECT * FROM vendas
--- WHERE data_pedido >= '2021-12-01' AND data_pedido <= '2021-12-31'
--- WHERE data_pedido < '2021-12-01' OR data_pedido > '2021-12-31'
-WHERE data_pedido BETWEEN '2021-12-01' AND '2021-12-31'
-ORDER BY data_pedido ASC, valor_total DESC;
+-- SELECT * FROM vendas
+--  -- WHERE data_pedido >= '2021-12-01' AND data_pedido <= '2021-12-31'
+--  -- WHERE data_pedido < '2021-12-01' OR data_pedido > '2021-12-31'
+-- WHERE data_pedido BETWEEN '2021-12-01' AND '2021-12-31'
+-- ORDER BY data_pedido ASC, valor_total DESC;
 
 
-SELECT * FROM vendas
-WHERE data_pedido < '2022-01-01'
-ORDER BY data_pedido ASC, valor_total DESC;
+-- SELECT * FROM vendas
+-- WHERE data_pedido < '2022-01-01'
+-- ORDER BY data_pedido ASC, valor_total DESC;
 
-SELECT * FROM vendas
-WHERE qtd_itens BETWEEN 5 AND 10
-ORDER BY qtd_itens ASC;
+-- SELECT * FROM vendas
+-- WHERE qtd_itens BETWEEN 5 AND 10
+-- ORDER BY qtd_itens ASC;
 
 
-select * from vendas
-where qtd_itens > 2
-order by qtd_itens asc
-limit 5 offset 13;
+-- select * from vendas
+-- where qtd_itens > 2
+-- order by qtd_itens asc
+-- limit 5 offset 13;
 
-select * from vendas
-where cliente = 'Arnold Schwarzenegger';
+-- select * from vendas
+-- where cliente = 'Arnold Schwarzenegger';
 
-update vendas
-set cliente = 'Arnold 2 Schwarzenegger'
-where cliente = 'Arnold Schwarzenegger'
+-- update vendas
+-- set cliente = 'Arnold 2 Schwarzenegger'
+-- where cliente = 'Arnold Schwarzenegger'
 
-select * from vendas;
+-- select * from vendas;
 
-UPDATE vendas
-SET data_pagamento = null;
+-- UPDATE vendas
+-- SET data_pagamento = null;
 
-UPDATE vendas
-SET entregue = true
-WHERE data_entrega is not null;
+-- UPDATE vendas
+-- SET entregue = true, data_entrega = '2022-01-27'
+-- WHERE numero_pedido BETWEEN 20 and 100;
 
-UPDATE vendas
-SET data_pagamento = data_entrega
-WHERE data_entrega is not null
-AND
-pago is true;
+-- UPDATE vendas
+-- SET data_pagamento = data_entrega
+-- WHERE data_entrega is not null
+-- AND
+-- pago is true;
 
-DELETE FROM vendas
-WHERE data_pagamento is null 
-      and pago is false 
-      and data_entrega is not null;
+-- DELETE FROM vendas
+-- WHERE data_pagamento is null 
+--       and pago is false 
+--       and data_entrega is not null;
 
-select cliente, pago, data_pagamento, entregue, data_entrega from vendas;
+-- select cliente, pago, data_pagamento, entregue, data_entrega from vendas;
---
+++ m02s07 - sql basico/14-criando-schemas.sql
@@ -0,0 +1,77 @@
+-- create database breadinhouse;
+
+-- create schema fabrica;
+-- create schema pessoal;
+
+-- CREATE TYPE status_forno
+-- AS ENUM ('assando','descansando','aquecer','desligado');
+
+-- create type status_cozinha_enum as enum ('ativo', 'manutenção')
+-- create type turno_enum as enum ('manhã', 'tarde', 'noite')
+
+-- CREATE TABLE fabrica.fornos (
+--   forno_id SERIAL PRIMARY KEY,
+--   hora_inicio TIME NOT NULL, -- not null
+--   qtd_paes INT NOT NULL CHECK (qtd_paes > 0), -- > 20 not null
+--   status_forno status_forno NOT NULL, -- not null
+--   temperatura FLOAT NOT NULL CHECK (temperatura >= 0), -- > 0 not null
+--   tipo_pao VARCHAR(100) NOT NULL UNIQUE, -- unique not null
+--   operador VARCHAR(80) NOT NULL -- not null
+-- );
+
+-- create table fabrica.cozinhas (
+--   cozinha_id SERIAL PRIMARY KEY,
+--   responsavel VARCHAR(80) NOT NULL,
+--   nome_batismo VARCHAR(80),
+--   horario_abertura TIME,
+--   horario_fechamento TIME,
+--   status_cozinha status_cozinha_enum,
+--   data_ultima_manutencao TIMESTAMP
+-- );
+
+-- create table pessoal.supervisores (
+--   supervisor_id SERIAL PRIMARY KEY,
+--   nome VARCHAR(80) NOT NULL,
+--   cargo VARCHAR(80) DEFAULT 'Supervisor',
+--   turno turno_enum
+-- );
+
+-- INSERT INTO fabrica.fornos
+-- (hora_inicio, qtd_paes, status_forno, temperatura, tipo_pao, operador)
+-- VALUES 
+-- ('04:00', 100, 'aquecer', 180.6, 'Pão de queijo', 'Vitor'),
+-- ('07:30', 500, 'aquecer', 200, 'Massa fina', 'Victor');
+
+
+--   cozinha_id SERIAL PRIMARY KEY,
+--   responsavel VARCHAR(80) NOT NULL,
+--   nome_batismo VARCHAR(80),
+--   horario_abertura TIME,
+--   horario_fechamento TIME,
+--   status_cozinha status_cozinha_enum,
+--   data_ultima_manutencao TIMESTAMP
+
+-- insert into fabrica.cozinhas
+-- (responsavel, nome_batismo, horario_abertura, horario_fechamento, status_cozinha)
+-- values
+-- ('João da Silva', 'Primavera', '04:00:00', '18:00:00', 'ativo');
+
+-- insert into pessoal.supervisores
+-- (nome, turno)
+-- VALUES
+-- ('Michael', 'manhã'),
+-- ('Sabrina', 'tarde'),
+-- ('Priscilla', 'manhã'),
+-- ('Lucas', 'noite'),
+-- ('Vitor Pedra', 'manhã');
+
+alter table pessoal.supervisores
+add column nome_cozinha VARCHAR(80);
+
+select * from fabrica.cozinhas;
+select * from pessoal.supervisores;
+
+create view nome_batismo_primavera as (SELECT nome_batismo FROM fabrica.cozinhas LIMIT 1);
+
+update pessoal.supervisores
+set nome_cozinha = (SELECT * FROM nome_batismo_primavera);
